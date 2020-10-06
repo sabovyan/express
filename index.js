d313bfe306f9f7baa1c8992afa6197fc29891bab
sabovyan <abovyan.sargis90@gmail.com>
2020-10-06 12:49:01 +0400

    💚ci(routes) add router with express

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const express = require('express');
+const pug = require('pug');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/home' || req.url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream(path.join(__dirname, '/index.html')).pipe(res);
-  } else if (req.url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream(path.join(__dirname, '/about.html')).pipe(res);
-  } else if (req.url === '/api/find%me') {
-    res.writeHead(200, { 'content-type': 'application/json' });
-    const data = {
-      date: new Date(),
-      text: 'yep this is the hidden text',
-      heading: "Ain't nobody",
-    };
-    res.end(JSON.stringify(data));
-  } else {
-    res.writeHead(404, { 'content-type': 'text/html' });
-    fs.createReadStream(path.join(__dirname, '/404.html')).pipe(res);
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, '/index.pug'));
+});
+
+app.get('/profile/:name', (req, res) => {
+  // res.render(path.join(__dirname, '/profile.pug'), );
+
+  res.render(path.join(__dirname, '/profile.pug'), { name: req.params.name });
+});
+app.use(function (req, res, next) {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.render(path.join(__dirname, '/404.pug'), { url: req.url });
   }
 });
 
-server.listen(5000, '127.0.0.1');
+app.listen(5000);
+
 console.log('yo');
